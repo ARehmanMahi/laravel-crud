b1fc82af9b61d8a16f36be88624633add5739f70
faisal <aimsbrains073@gmail.com>
2021-12-24 12:42:34 +0500

    Image Uploading and Axios

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -7,7 +7,6 @@
 
     {{-- datatables-bs5 css --}}
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.css"/>
-<<<<<<< HEAD
     {{-- datatables-bs5 js --}}
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
 
@@ -42,82 +41,6 @@
         </table>
     </div>
 
-    <script type="text/javascript">
-        $(document).ready(function () {
-            let table = $('.datatable').DataTable({
-                processing: true,
-                serverSide: true,
-                bFilter: false,
-                ajax: "{{ route('members.list') }}",
-                columns: [
-                    {
-                        data: "id", name: 'id', orderable: false, searchable: false,
-                        render: function (data, type, row, meta) {
-                            return meta.row + meta.settings._iDisplayStart + 1;
-                        }
-                    },
-                    {data: 'first_name', name: 'first_name', orderable: false, searchable: false},
-                    {data: 'last_name', name: 'last_name', orderable: false, searchable: false},
-                    {data: 'email', name: 'email', orderable: false, searchable: false},
-                    {data: 'info', name: 'info', orderable: false, searchable: false},
-                    {data: 'image_path', name: 'image_path', orderable: false, searchable: false,
-                        render: function (data, type, row, meta) {
-                            return `
-                            <a href="${row.image_path}" target="_blank"><i class="fa fa-eye"></i> Image</a>
-                        `;
-                        }
-                    },
-                    {data: 'is_active', name: 'is_active', orderable: false, searchable: false},
-                    {data: 'created_at', name: 'created_at', orderable: false, searchable: false},
-                    {
-                        data: "action", name: 'action', orderable: false, searchable: false,
-                        render: function (data, type, row, meta) {
-                            let show_url = "{{ route('members.show', ['member' => ':id']) }}";
-                            show_url = show_url.replace(':id', row.id);
-
-                            let edit_url = "{{ route('members.edit', ['member' => ':id']) }}";
-                            edit_url = edit_url.replace(':id', row.id);
-
-                            return `
-=======
-</head>
-<body>
-
-<div class="container mt-5">
-    <h2 class="mb-4">Listing (Datatables - Server Paginated)</h2>
-
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    <h3><a class="btn btn-primary" href="{{ route('members.create') }}">Create</a></h3>
-
-    <table class="table table-bordered table-striped datatable" aria-describedby="Member listing">
-        <thead>
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Info</th>
-            <th scope="col">Is active</th>
-            <th scope="col">Created At</th>
-            <th scope="col">Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-</div>
-
-{{-- jQuery-3 js --}}
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-{{-- bootstrap-5 js --}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
 {{-- datatables-bs5 js --}}
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
 
@@ -126,7 +49,7 @@
         return 'Hi Im Testing Row And my Index is 1'+ '<br>'+
             'Hi Im Testing Row And my Index is 2: '
     }
-    
+
     $(function () {
         let table = $('.datatable').DataTable({
             processing: true,
@@ -144,6 +67,13 @@
                 {data: 'last_name', name: 'last_name', orderable: false, searchable: false},
                 {data: 'email', name: 'email', orderable: false, searchable: false},
                 {data: 'info', name: 'info', orderable: false, searchable: false},
+                {data: "image_path" , name: 'image_path', orderable: false, searchable: false,
+                    render : function(data, type, row, meta) {
+                        return `
+                        <a href="${row['image_path']}" target="_blank"><i class="fa fa-eye"></i>View</a>
+                           `;
+                    }
+                },
                 {data: 'is_active', name: 'is_active', orderable: false, searchable: false},
                 {data: 'created_at', name: 'created_at', orderable: false, searchable: false},
                 {data: "action" , name: 'action', orderable: false, searchable: false,
@@ -155,7 +85,7 @@
                         edit_url = edit_url.replace(':id', row.id);
 
                         return `
->>>>>>> 76b2a0ce162d7802a07cb1c05010ddc927086dc8
+
                             <a href="${show_url}" class="edit btn btn-primary btn-sm"><i class="fa fa-eye"></i></a> |
                             <a href="${edit_url}" class="edit btn btn-success btn-sm"><i class="fa fa-edit"></i></a> |
                             <button type="button" class="destroy btn btn-danger btn-sm" id="deleteRecord" onclick="deleteRecord(${row.id})"><i class="fa fa-trash"></i></button> |
